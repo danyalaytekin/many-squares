8b5a5f79594cacb0dbd90b0638690894f788ef7c
Danyal Aytekin <danyal@fubase.com>
2017-09-17 23:23:03 +0100

    Remove redundant extra line

--- src/ui/table-renderer.js
+++ src/ui/table-renderer.js
@@ -1,6 +1,5 @@
 export function render (areas) {
-    let rowHtml = '';
-    rowHtml += `<tr><th>Group containing</th><th>Area (pixels squared)</th></tr>`;
+    let rowHtml = `<tr><th>Group containing</th><th>Area (pixels squared)</th></tr>`;
     for (let i = 0; i < areas.length; ++i) {
         rowHtml += `<tr><td>${areas[i].group[0].innerHTML}</td><td>${areas[i].area}</td></tr>`;
     }
