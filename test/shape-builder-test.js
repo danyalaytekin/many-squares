e3a69b7d4334f8136ed6f71b7bfaa110c17ce2cf
Danyal Aytekin <danyal@fubase.com>
2017-09-17 19:26:20 +0100

    Add initial failing tests for `shape-builder`

---
+++ test/shape-builder-test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+// const shapeBuilder = require('../src/ui/shape-builder');
+
+describe('Shape builder', function () {
+    describe('#setRandomPosition(element)', function() {
+        beforeEach(function () {
+            this.element = {
+                style: {}
+            };
+        });
+        
+        it('should set the element\'s left style', function () {
+            assert.ok (false);
+        });
+
+        it('should set the element\'s right style', function () {
+            assert.ok (false);
+        });
+    });
+});
