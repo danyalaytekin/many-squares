796190e3dc01f2e8f25e1244f2414e12a2312d84
Danyal Aytekin <danyal@fubase.com>
2017-09-18 06:07:30 +0100

    Test that styles are set by shapeBuilder.setRandomPosition

--- test/shape-builder-test.js
+++ test/shape-builder-test.js
@@ -1,20 +1,28 @@
 import assert from 'assert';
-// import * as shapeBuilder from '../src/ui/shape-builder';
+import * as shapeBuilder from '../src/ui/shape-builder';
 
 describe('Shape builder', function () {
     describe('#setRandomPosition(element)', function() {
         beforeEach(function () {
+            shapeBuilder.init ({
+                limits: {
+                    width: 0,
+                    height: 0
+                }
+            });
             this.element = {
                 style: {}
             };
         });
         
         it('should set the element\'s left style', function () {
-            assert.ok (false);
+            shapeBuilder.setRandomPosition (this.element);
+            assert.notStrictEqual (this.element.style.left, undefined);
         });
 
-        it('should set the element\'s right style', function () {
-            assert.ok (false);
+        it('should set the element\'s top style', function () {
+            shapeBuilder.setRandomPosition (this.element);
+            assert.notStrictEqual (this.element.style.top, undefined);
         });
     });
 });
