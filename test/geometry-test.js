e86a5144237b5f2e08c022169d10f0b92463f244
Danyal Aytekin <danyal@fubase.com>
2017-09-18 09:10:32 +0100

    Add test coverage for `createPaintableCanvasForGroup`

--- test/geometry-test.js
+++ test/geometry-test.js
@@ -2,6 +2,42 @@ import assert from 'assert';
 import * as geometry from '../src/model/geometry';
 
 describe('Geometry', function () {
+    describe('#createPaintableCanvasForGroup(rectangle)', function () {
+        function createSmallRectangle () {
+            return {
+                left: 1,
+                right: 2,
+                top: 1,
+                bottom: 2
+            };
+        }
+
+        it('should return an empty array, when a rectangle with zero area is provided', function () {
+            const rectangle = {
+                left: 1,
+                right: 1,
+                top: 1,
+                bottom: 1
+            };
+            const canvas = geometry.createPaintableCanvasForGroup (rectangle);
+            assert.strictEqual(canvas.length, 0);
+        });
+
+        it('should create a two-dimensional array, when a rectangle with non-zero area is provided', function () {
+            const rectangle = createSmallRectangle ();
+            const canvas = geometry.createPaintableCanvasForGroup (rectangle);
+            assert.notStrictEqual(canvas.length, 0);
+            assert.notStrictEqual(canvas[0].length, 0);
+        });
+
+        it('should ignore a rectangle\'s position', function () {
+            const rectangle = createSmallRectangle ();
+            const canvas = geometry.createPaintableCanvasForGroup (rectangle);
+            assert.strictEqual(canvas.length, 1);
+            assert.strictEqual(canvas[0].length, 1);
+        });
+    });
+
     describe('#countPaintedPixels(canvas)', function() {
         it('should return zero for an empty canvas', function () {
             assert.strictEqual (
