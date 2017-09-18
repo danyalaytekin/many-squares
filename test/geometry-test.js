c12c1173bafe16f08a6939f5b5e4a7fb2d4ff662
Danyal Aytekin <danyal@fubase.com>
2017-09-18 07:36:06 +0100

    Expand test coverage of `countPaintedPixels`

--- test/geometry-test.js
+++ test/geometry-test.js
@@ -5,26 +5,70 @@ describe('Geometry', function () {
     describe('#countPaintedPixels(canvas)', function() {
         it('should return zero for an empty canvas', function () {
             assert.strictEqual (
-                geometry.countPaintedPixels( [ [] ] ),
+                geometry.countPaintedPixels([ 
+                    [] 
+                ]),
                 0
             );
         });
 
-        it('should cope with a filled canvas', function () {
+        it('should cope with a canvas filled in one dimension', function () {
             assert.strictEqual (
-                geometry.countPaintedPixels( [ [1] ] ),
+                geometry.countPaintedPixels([
+                    [1]
+                ]),
                 1
             );
 
             assert.strictEqual (
-                geometry.countPaintedPixels( [ [0, 1, 1, 0] ] ),
+                geometry.countPaintedPixels([ 
+                    [0, 1, 1, 0]
+                ]),
                 2
             );
 
             assert.strictEqual (
-                geometry.countPaintedPixels( [ [1, 0, 1, 0, 1, 0, 1]] ),
+                geometry.countPaintedPixels([
+                    [1, 0, 1, 0, 1, 0, 1]
+                ]),
                 4
             );
-        });  
+        });
+
+        it('should cope with a canvas filled in two dimensions', function () {
+            assert.strictEqual (
+                geometry.countPaintedPixels([
+                    [0, 0],
+                    [0, 1]
+                ]),
+                1
+            );
+
+            assert.strictEqual (
+                geometry.countPaintedPixels([
+                    [1, 0],
+                    [0, 1]
+                ]),
+                2
+            );
+
+            assert.strictEqual (
+                geometry.countPaintedPixels([
+                    [1, 0, 1],
+                    [0, 0, 0],
+                    [1, 1, 1],
+                ]),
+                5
+            );
+
+            assert.strictEqual (
+                geometry.countPaintedPixels([
+                    [1, 1, 1],
+                    [1, 1, 1],
+                    [1, 1, 1]
+                ]),
+                9
+            );            
+        });
     });
 });
